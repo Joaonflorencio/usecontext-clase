3d4b44bf0316b145f9524bb98ef29bc00c9657e6
João Neto <jflorencioadv@gmail.com>
2024-04-04 12:08:23 +0200

    proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-// App.js
 import React from 'react';
+import { LanguageProvider } from './LanguageContext';
+import LanguageSelector from './LanguageSelector';
+import Greeting from './Greeting';
 
+// The main component of the app, wrapping everything in LanguageProvider for context
 const App = () => {
   return (
-    <>useContext</>
+    <LanguageProvider>
+      <div>
+        <h1>Language Selector App</h1>
+        <LanguageSelector /> {/* Component to select the language */}
+        <Greeting /> {/* Component to display a greeting in the selected language */}
+      </div>
+    </LanguageProvider>
   );
 };
 
